12d7726ae081f40d3905906484942dcf93cf9548
Nicole Yang <99329229+nini0902@users.noreply.github.com>
2025-03-05 06:24:54 +0800

    Update f277_嘿嘿想不到吧.py

--- APCS題單/f277_嘿嘿想不到吧.py
+++ APCS題單/f277_嘿嘿想不到吧.py
@@ -2,14 +2,11 @@ n = int(input())
 stu = []
 
 for line in range(n):
-  p = list(map(str, input().split()))
-  name = p[0]
-  c = int(p[1])
-  num = int(p[2])
-  sen = p[3]
-  stu.append([c, num, name, sen])
+  p = input().split()
+  c, num = int(p[1]), int(p[2])
+  stu.append([c, num, p[0], p[3]])
   
-stu.sort(key = lambda x:(x[0], x[1]))
+stu.sort()
 
 for i in range(n):
   print(*stu[i][:3])
